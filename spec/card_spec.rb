81d8a7b921e632e05146ebcccaef17141808aa36
Dade Niman <dade.niman@gmail.com>
2023-09-04 13:18:38 +0300

    #info

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,12 +1,23 @@
 class Card
-  # пики вини червы бубны
-  SUITS = %w{ \u{2661} \u{2662} \u{2664} \u{2667} }.freeze
+  # червы бубны пики вини 
+  SUITS = %W{ \u{2661} \u{2662} \u{2664} \u{2667} }.freeze
 
   attr_reader :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
+  end
+
+  def info
+    case rank
+    when 1 then "A#{suit}"
+    when 11 then "J#{suit}"
+    when 12 then "Q#{suit}"
+    when 13 then "K#{suit}"
+    else
+      "#{rank}#{suit}"
+    end
   end
 
 end
@@ -20,6 +31,46 @@ RSpec.describe Card do
 
   it '#suit' do
     expect(card.suit).to eq(Card::SUITS.first)
+  end
+
+
+  describe '#info' do
+    context 'when rank is 1' do
+      it "prints A#{Card::SUITS.first}" do
+        card = Card.new(1, Card::SUITS.first)
+        expect(card.info).to eq("A#{Card::SUITS.first}")
+      end
+    end
+
+    context 'when rank is 11' do
+      it "prints J#{Card::SUITS.first}" do
+        card = Card.new(11, Card::SUITS.first)
+        expect(card.info).to eq("J#{Card::SUITS.first}")
+      end
+    end
+    
+    context 'when rank is 12' do
+      it "prints Q#{Card::SUITS.first}" do
+        card = Card.new(12, Card::SUITS.first)
+        expect(card.info).to eq("Q#{Card::SUITS.first}")
+      end
+    end
+    
+    context 'when rank is 13' do
+      it "prints K#{Card::SUITS.first}" do
+        card = Card.new(13, Card::SUITS.first)
+        expect(card.info).to eq("K#{Card::SUITS.first}")
+      end
+    end
+    
+    context 'when rank from 2 till 10' do
+      2.upto(10) do |rank|
+        card = Card.new(rank, Card::SUITS.first)
+        it "prints #{rank}#{Card::SUITS.first}" do
+          expect(card.info).to eq("#{rank}#{Card::SUITS.first}")
+        end
+      end
+    end
   end
 
 end
