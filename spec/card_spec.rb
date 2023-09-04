52806420cd0fe7dd8c4e775a826d48cb8c7a803f
Dade Niman <dade.niman@gmail.com>
2023-09-04 12:22:50 +0300

    #rank

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,18 +1,25 @@
 class Card
-  attr_reader :rank
+  # пики вини червы бубны
+  SUITS = %w{ \u{2661} \u{2662} \u{2664} \u{2667} }.freeze
 
-  def initialize(rank)
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
     @rank = rank
+    @suit = suit
   end
 
 end
 
 RSpec.describe Card do
+  let(:card) { Card.new(2, Card::SUITS.first) }
   
   it '#rank' do
-    card = Card.new(2)
-
     expect(card.rank).to eq(2)
+  end
+
+  it '#suit' do
+    expect(card.suit).to eq(Card::SUITS.first)
   end
 
 end
