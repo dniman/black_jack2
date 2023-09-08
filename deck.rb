45e3cc5a2910d27ee26c9d99f04ccfa42bf60748
Dade Niman <dade.niman@gmail.com>
2023-09-08 14:37:05 +0300

    shuffle the deck during creating

--- deck.rb
+++ deck.rb
@@ -1,14 +1,30 @@
 require './card'
+require 'byebug'
 
 class Deck
   attr_reader :cards
 
   RANKS = *(1..13)
 
-  def initialize
-    @cards = Card::SUITS.product(RANKS).map { |suit, rank| Card.new(rank, suit) }
+  def self.build
+    Card::SUITS.product(RANKS).map { |suit, rank| Card.new(rank, suit) }
   end
 
-  def shuffle
+  def initialize
+    @cards = self.class.build.shuffle
+  end
+
+  def ==(other)
+    if other.nil? || !other.instance_of?(Deck)
+      return false
+    else
+      index = 0
+      while index < cards.size do
+        return unless cards[index].rank == other.cards[index].rank && cards[index].suit == other.cards[index].suit
+        index += 1
+      end
+
+      true
+    end
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,7 +2,35 @@ require './deck'
 require './card'
 
 RSpec.describe Deck do
-  it "has 52 cards" do
-    expect(subject.cards.size).to eq(52)
+  subject { described_class.new }
+
+  describe '.build' do
+    it "creates array of 52 cards" do
+      expect(described_class.build.size).to eq(52)
+    end
+  end
+
+  it "has shuffled cards" do
+    expect_any_instance_of(Array).to receive(:shuffle)
+    subject
+  end
+
+  context 'equality' do
+    let(:deck) { described_class.new }
+
+    describe 'comparing with the same deck' do
+      it 'is equal' do
+        cards = deck.cards.clone.map(&:clone)
+        allow(subject).to receive(:cards).and_return(cards)
+
+        expect(subject).to eq(deck)
+      end
+    end
+
+    describe 'comparing with different deck' do
+      it 'is not equal' do
+        expect(subject).to_not eq(deck)
+      end
+    end
   end
 end
